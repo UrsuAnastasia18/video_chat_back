e26499248d52e3fdc911a07a3b95353081f48a06
Anastasia <anastasiaursu76@gmail.com>
2026-02-05 00:26:18 +0200

    Reusable Modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,11 +3,15 @@
 import { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
   const router = useRouter();
   const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
 
+  const createMeeting = () => {
+
+  }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
         <HomeCard 
@@ -37,6 +41,16 @@ const MeetingTypeList = () => {
         description="Join via invitation link"
         handleClick={() => setMeetingState ('isJoiningMeeting')}
         className="bg-yellow-1"
+        />
+
+        <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
+
         />
     </section>
   )

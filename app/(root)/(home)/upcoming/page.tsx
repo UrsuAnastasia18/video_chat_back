b46802034936f7a8b3277e8e8359ee7cea9fd843
Anastasia <anastasiaursu76@gmail.com>
2026-03-10 00:38:20 +0200

    corectare eroare pagina upcoming

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,14 +1,14 @@
-import CallList from '@/components/CallList'
-import React from 'react'
+import CallList from "@/components/CallList";
+import React from "react";
 
 const Upcoming = () => {
   return (
-    <section className= "flex size-full flex-col gap-10 text-black">
-      <h1 className="text-3xl font-bold">
-        Upcoming
-      </h1>
-      </section>
-  )
-}
- <CallList type="upcoming" />
-export default Upcoming
+    <section className="flex size-full flex-col gap-10 text-black">
+      <h1 className="text-3xl font-bold">Upcoming</h1>
+
+      <CallList type="upcoming" />
+    </section>
+  );
+};
+
+export default Upcoming;

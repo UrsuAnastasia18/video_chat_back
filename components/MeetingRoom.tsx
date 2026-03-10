6340590efb0665243ad93164dd38a82640149de1
Anastasia <anastasiaursu76@gmail.com>
2026-03-10 09:33:53 +0200

    corectare eroare meeting room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -52,7 +52,7 @@ const MeetingRoom = () => {
                 </div>
             </div>
 
-            <div className="fixed botton-0 flex w-full items-center justify-center gap-5 flex-wrap">
+            <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
                 <CallControls onLeave={() => router.push('/') }/>
 
                 <DropdownMenu>
